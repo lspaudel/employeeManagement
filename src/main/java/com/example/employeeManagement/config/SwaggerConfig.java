26ce563897f59a1191da5b5a4161722487403669
Laxman Paudel <laxmansharmapaudel@gmail.com>
2025-12-09 10:57:21 +0545

    Add Swagger/OpenAPI

---
+++ src/main/java/com/example/employeeManagement/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.example.employeeManagement.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Employee Management API")
+                        .version("1.0")
+                        .description("API documentation for the employee management system")
+                        .contact(new Contact()
+                                .name("Laxman Paudel")
+                                .email("laxmansharmapaudel@gmail.com")
+                                .url("https://github.com/lspaudel"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0"))
+
+
+                        );
+    }
+    @Bean
+    public GroupedOpenApi employeeApiGroup() {
+        return GroupedOpenApi.builder()
+                .group("Employee API")
+                .pathsToMatch("/api/employees/**")
+                .build();
+    }
+}
